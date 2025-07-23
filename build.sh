f1849698f48e3250230f01b3f452699e0600d0e6
1F10240223 <s1f102402237@iniad.org>
2025-07-23 15:30:58 +0900

    いい加減にしてください

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-python manage.py migrate
-echo "--- Django settings test start ---"
-python manage.py shell -c "from django.conf import settings; print('SECRET_KEY exists:', hasattr(settings, 'SECRET_KEY'))"
-echo "--- Django settings test end ---"
+python manage.py collectstatic --no-input
+python manage.py migrate 
