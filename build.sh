7cba3f6a6a761689dc9e643b11b3cb1a3e853299
1F10240223 <s1f102402237@iniad.org>
2025-07-23 15:25:21 +0900

    そんなこともあるよな。誰にだってミスがある。2

--- build.sh
+++ build.sh
@@ -5,3 +5,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+echo "--- Django settings test start ---"
+python manage.py shell -c "from django.conf import settings; print('SECRET_KEY exists:', hasattr(settings, 'SECRET_KEY'))"
+echo "--- Django settings test end ---"
