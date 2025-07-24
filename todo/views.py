dbdd114e139fac0ecb99630f226e8440f87033c0
1F10240027 <s1F102400279@iniad.org>
2025-07-24 23:13:18 +0900

    editページのデザイン追加

--- todo/views.py
+++ todo/views.py
@@ -39,7 +39,14 @@ def update(request, task_id):
     except Task. DoesNotExist:
         raise Http404("Task does not exist")
     if request.method == 'POST':
+        print(request.POST)
         task.title = request.POST['title']
+        compstate = request.POST.get('completed', 'off')
+        if compstate == 'on':
+            compstate = True
+        elif compstate == 'off':
+            compstate = False
+        task.completed = compstate
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
         return redirect(detail, task_id)
